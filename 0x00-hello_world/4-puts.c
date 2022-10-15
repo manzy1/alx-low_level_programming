e4c5026b14531ea53d0d1d07215a65ab0d53cee3
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-10-15 09:39:56 -0700

    a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>  
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * return: always 0
+ * Description: 'a C program that prints'
+ *
+ * return: always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
