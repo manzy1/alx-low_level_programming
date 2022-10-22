bf2335141aff52b382fda4be236d2554d1d5ed3a
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-10-22 03:52:25 -0700

    The “Fizz-Buzz test”

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - print fizzbuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	printf("1");
+
+	for (n = 2; n < 100; n++)
+	{
+		printf(" ");
+		if (n % 3 == 0)
+			print("fizz");
+		if (n % 5 == 00
+				print("buzz"0
+		if (n % 3 != 0 && n % 5 != 0)
+		print("%d%", n);
+	}
+
+	printf('\n');
+	return (0);
+}
