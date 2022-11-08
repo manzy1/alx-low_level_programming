5cd05df106a48af872f9645422dfca54c936c3ef
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-08 13:23:36 -0800

    a function that prints a string

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: the character to be printed
+ * Return: om success 1
+ * on error -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
