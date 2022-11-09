9f7fdfbb9ca9ba2859c9470435a8a670e29571b8
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-08 17:24:17 -0800

    standard library

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a sring of bytes from string
+ *            source into destination
+ * @dest: buffer storing the string copy
+ * @src:the source string
+ * @n:max nummber of byte copied
+ * Return: returns a pointer to resulting dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
