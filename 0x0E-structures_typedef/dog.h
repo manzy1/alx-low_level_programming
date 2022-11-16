ea8a72179fa6f7d3f51fe5ba4bde6d23d2379b20
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-15 18:06:11 -0800

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,16 @@
  * @owner: name of the owner of the dog
  * Description: Defines metadata about the dog and type of dog's struct
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-/* Additional function prototypes */
-int _putchar(char c);
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG_H*/
