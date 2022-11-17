a9cdd178693b58dd3fd053d41e1cc31160c26513
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-16 18:48:21 -0800

    function that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,14 +9,17 @@
  * @pointer: pointer to function used
  * Return: 0
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!array || !action)
+	if (array == NULL)
 		return;
 
-	for (i - 0; i < size; i++)
+	if (action == NULL)
+		return;
+	
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
 
