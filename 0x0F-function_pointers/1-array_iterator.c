d90ba0f0d332fc6c1f66f23b0714acf555ef353f
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-16 18:41:51 -0800

    function that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * int_index - return index place if comparison = true, else -1
+ * array_iterator - executes a function given as a parameter
  * @array: array
  * @size: size of elements in array
- * @cmp: pointer to func of one of the 3 in main
+ * @pointer: pointer to function used
  * Return: 0
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 	if (!array || !action)
 		return;
 
-	for (i=0; i < size; i++)
+	for (i - 0; i < size; i++)
 		action(array[i]);
 }
 
