5c284a37664af3e34ba8d3426c6b7d17b79c6422
emmanuel mziya <mziyaemmanuel@gmail.com>
2022-11-16 18:31:18 -0800

    function that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,22 +11,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array == NULL)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	if (cmp == NULL)
-		return (-1);
+	if (!array || !action)
+		return;
 
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
-
-	return (-1);
+	for (i=0; i < size; i++)
+		action(array[i]);
 }
 
 
