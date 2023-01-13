39cb2e5b3173f788f8ad90d8f5c781c2f58f7630
emmanuel mziya <mziyaemmanuel@gmail.com>
2023-01-12 16:35:42 -0800

    print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -18,3 +18,25 @@ size_t print_dlistint(const dlistint_t *h)
 
 	return (num_nodes);
 }
+
+
+
+
+vi 1-dlistint_len.c
+
+#include "lists.h"
+
+/**
+ * dlistint_len - return length of linkedlist
+ * @h: doubly linkedlist
+ * Return: number of elements in a linked dlistint_t list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+
+	while (h)
+		h = h->next, length++;
+
+	return (length);
+}
